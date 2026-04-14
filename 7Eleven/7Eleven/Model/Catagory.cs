a60d40b46217d8ed8614f38923acbc9bc4a05f6d
Mohamedawad-code <awadmohamed7000@gmail.com>
2026-04-14 12:57:47 +0200

    Add Coworker, Catagory, and product classes with constructors

---
+++ 7Eleven/7Eleven/Model/Catagory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Eleven.Model
+{
+    public class Catagory
+    {
+        public string FoodName { get; set; }
+        public FoodCategory FoodType { get; set; }
+
+        public Catagory (string foodname, FoodCategory foodtype)
+        {
+            FoodName = foodname;
+            FoodType = foodtype;
+
+        }
+
+       
+    }
+
+    public enum FoodCategory
+    {
+        Bread,
+        Dessert,
+        Sandwhiches,
+        Pastry,
+        FriedFood,
+
+    }
+}
---
+++ 7Eleven/7Eleven/Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Eleven.Model
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public Guid ProductNO { get; set; } = Guid.NewGuid();
+        public DateTime TimeReceived { get; set; }
+        public DateTime ExpiringDate { get; set; }
+
+
+        public Product (string name, int amount, DateTime timeReceived, DateTime expiringdate)
+        {
+            Name = name;
+            Amount = amount;
+            TimeReceived = timeReceived;
+            ExpiringDate = expiringdate;
+
+        }
+
+    }
+}

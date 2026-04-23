aa2bcaf1df682f9818f6ff1ab4b2c23d83807980
Mohamedawad-code <awadmohamed7000@gmail.com>
2026-04-23 12:24:29 +0200

    Add Registerpaper and Depreciation ViewModels with CRUD methods

---
+++ 7Eleven/7Eleven/ViewModel/DepreciationViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _7Eleven.Model;
+
+namespace _7Eleven.ViewModel
+{
+    public class DepreciationViewModel
+    {
+        public List<Depreciation> _depreciation = new List<Depreciation>();
+
+        public Depreciation CreateDepreciation(DateTime newDate, Product newProductdep)
+        {
+            var newDepreciation = new Depreciation(newDate, newProductdep);
+
+            _depreciation.Add(newDepreciation);
+            return newDepreciation;
+        }
+
+        public void DeleteDepreciation(Guid id)
+        {
+            var depreciation = _depreciation.FirstOrDefault(d => d.ID == id);
+            if (depreciation is null)
+                throw new ArgumentException("Invalid or doesnt exist");
+
+            _depreciation.Remove(depreciation);
+        }
+
+        public Depreciation EditDepreciation(DateTime updatedDate, Product updatedProductdep, Guid id)
+        {
+            var updatedDepreciation = _depreciation.FirstOrDefault(d => d.ID == id);
+
+            updatedDepreciation.ProductDep = updatedProductdep;
+            updatedDepreciation.Date = updatedDate;
+
+            return updatedDepreciation;
+        }
+
+    }
+}
---
+++ 7Eleven/7Eleven/ViewModel/RegisterPaperViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _7Eleven.Model;
+
+namespace _7Eleven.ViewModel
+{
+    public class RegisterPaperViewModel
+    {
+        private List<RegisterPaper> _registerpaper = new List<RegisterPaper>();
+
+        public RegisterPaper CreateRegisterPaper(Coworker Newregisteredby
+            , List<Category> newCategories
+            , List<Depreciation> newDepreciations
+            , List<Product> newProducts
+            , DateTime newDate)
+        {
+            var newRegisterPaper = new RegisterPaper(Newregisteredby
+            , newCategories
+            , newDepreciations
+            , newProducts
+            , newDate);
+
+            return newRegisterPaper;
+
+        }
+
+        public void DeleteRegisterPaper(Guid id)
+        {
+            var registerPaper = _registerpaper.FirstOrDefault(r => r.ID == id);
+            if (registerPaper is null)
+                throw new ArgumentException("register_paper is null");
+
+            _registerpaper.Remove(registerPaper);
+        }
+
+      
+
+
+
+    }
+}

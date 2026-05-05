082df4f8341bea8ee74cb0edb137fecd605cec47
Lai Ly <laily-bape@hotmail.com>
2026-05-05 11:37:03 +0200

    Merge

--- 7Eleven/7Eleven/OverviewView.xaml.cs
+++ 7Eleven/7Eleven/OverviewView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _7Eleven;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,7 +32,43 @@ namespace _7Eleven
             var main = Application.Current.MainWindow as MainWindow;
             main.MainContent.Content = null;
 
+
+
+
         }
 
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            var main = Application.Current.MainWindow as MainWindow;
+            main.MainContent.Content = null;
+        }
+
+        private void RegisterProductView_Click(object sender, RoutedEventArgs e)
+        {
+            var main = Application.Current.MainWindow as MainWindow;
+            main.MainContent.Content = new RegisterProduct();
+        }
+
+        private void DeprecationView_Click(object sender, RoutedEventArgs e)
+        {
+            var main = Application.Current.MainWindow as MainWindow;
+            main.MainContent.Content = new DeprecationView();
+        }
+
+
+        private void OverviewView_Click(object sender, RoutedEventArgs e)
+
+        {
+            var main = Application.Current.MainWindow as MainWindow;
+            main.MainContent.Content = new OverviewView();
+
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
+
+
 }

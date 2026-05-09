5ce0a6999d4417c5990bc5988e612118b339eda4
Mohamedawad-code <awadmohamed7000@gmail.com>
2026-05-09 11:54:35 +0200

    Add RelayCommands class and Add Icommand to registerPaperViewModel

--- 7Eleven/7Eleven/ViewModel/RegisterPaperViewModel.cs
+++ 7Eleven/7Eleven/ViewModel/RegisterPaperViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Input;
 
 namespace _7Eleven.ViewModel
 {
@@ -10,6 +11,50 @@ namespace _7Eleven.ViewModel
     {
         private ObservableCollection<RegisterPaper> _registerpaper = new ObservableCollection<RegisterPaper>();
         private RegisterPaperRepository _repo = new RegisterPaperRepository();
+
+
+        // Input properties for RegisterPaper
+        public Product SelectedProduct { get; set; }
+        public Category SelectedCategory { get; set; }
+        public Coworker SelectedCoworker { get; set; }
+        public int NewAmount { get; set; }
+        public DateTime NewExpiringDate { get; set; }
+        public DateTime NewRegisterDate { get; set; }
+        public RegisterPaper SelectedRegisterPaper { get; set; }
+
+        public ICommand CreateRegisterPaperCommand { get; }
+        public ICommand DeleteRegisterPaperCommand { get; }
+
+        public void ExcuteCreateRegisterPaper() // WPF cant call methods with parameters, so we do dis
+        {
+            CreateRegisterPaper(SelectedProduct,
+               SelectedCategory,
+               NewAmount,
+               NewExpiringDate,
+               SelectedCoworker,
+               NewRegisterDate);
+        }
+
+        public void ExcuteDeleteRegisterPaper()
+        {
+            if (SelectedRegisterPaper is null)
+                return;
+
+            DeleteRegisterPaper(SelectedRegisterPaper.ID);
+        }
+
+        public RegisterPaperViewModel()
+        {
+            CreateRegisterPaperCommand = new RelayCommand(ExcuteCreateRegisterPaper);
+            DeleteRegisterPaperCommand = new RelayCommand(ExcuteDeleteRegisterPaper);
+
+            var result = _repo.GetAll();
+            _registerpaper = new ObservableCollection<RegisterPaper>(result);
+           
+
+        }
+
+        
 
         public RegisterPaper CreateRegisterPaper(Product Newproduct, Category Newcategory, int Newamount, DateTime NewexpiringDate, Coworker NewregisteredBy, DateTime NewregisterDate)
         {
